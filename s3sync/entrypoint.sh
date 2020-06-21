d5d08fa13fbdc096cc4595ad69d9192ccd58c7fc
xdassie <xdassie@gmail.com>
2020-06-21 19:11:58 +0200

    still failing on encryption key format errors

--- s3sync/entrypoint.sh
+++ s3sync/entrypoint.sh
@@ -1,4 +1,4 @@
  #!/bin/sh
  /mc/mc config host add  mys3 https://s3.amazonaws.com ${AWS_ACCESS_KEY} ${AWS_SECRET_KEY}
- /mc/mc mirror /sync/ mys3
+ /mc/mc mirror --encrypt-key mys3/sync=${AWS_ENCRYPTION_KEY}  /sync/ mys3/sync
  
